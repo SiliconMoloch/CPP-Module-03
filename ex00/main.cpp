a1472f4fadd408e159e22bb5d193424c8b2764de
Yazid Bokhari <yabokhar@z1r6p1.42lyon.fr>
2025-09-24 17:04:31 +0200

    Ex00 ok

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,15 +4,12 @@ int	main(void)
 
 {
 	ClapTrap Erwann("Erwann");
-	ClapTrap tempYazid;
-	ClapTrap Yazid("Yazid");
-
-	tempYazid = Yazid;
 	Erwann.attack("Yazid");
 	Erwann.takeDamage(5);
 	Erwann.beRepaired(3);
 	Erwann.takeDamage(15);
 	Erwann.attack("Yazid");
+	Erwann.takeDamage(50);
 
 	return (0);
 }
